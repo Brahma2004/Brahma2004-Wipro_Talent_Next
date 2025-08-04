db1bb4b0123ac96fd688683ad3ae1b78a0b6ba13
Brahma K <167072591+Brahma2004@users.noreply.github.com>
2025-08-04 07:10:07 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,6 +1,6 @@
 public class Fibonacci {
     public static void main(String[] args) {
-        int n = 15; // Number of terms to display
+        int n = 15; 
         int a = 0, b = 1;
         System.out.println("Fibonacci Series:");
         for (int i = 1; i <= n; i++) {
