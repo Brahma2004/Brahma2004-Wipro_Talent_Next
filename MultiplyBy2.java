694fa9f92d4acb55eeeed92f1b6db8dc21d8b2e0
Brahma K <kbrahma114@gmail.com>
2025-08-10 07:12:26 +0530

    Add files via Update

---
+++ MultiplyBy2.java
@@ -0,0 +1,7 @@
+public class MultiplyBy2 {
+    public static void main(String[] args) {
+        int num = 3;
+        int result = num << 1;
+        System.out.println("Multiplied by 2: " + result);
+    }
+}
